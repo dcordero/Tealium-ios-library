6fe798eb85f8f44a5d953cabc728c6987aabef65
George Webster <george.webster@tealium.com>
2014-09-15 11:01:49 -0700

    corrected framework paths for movie player sample. updated "blank" samples for both compact and full libraries use new Xcode 6 default "Single View" template without a PCH file.

--- TealiumFull/Samples+Full/BlankApp+TealiumFullLibrary/BlankApp+TealiumFullLibrary/AppDelegate.h
+++ TealiumFull/Samples+Full/BlankApp+TealiumFullLibrary/BlankApp+TealiumFullLibrary/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  BlankApp+TealiumFullLibrary
 //
-//  Created by Jason Koo on 7/1/14.
-//  Copyright (c) 2014 Tealium. All rights reserved.
+//  Created by George Webster on 9/15/14.
+//  Copyright (c) 2014 tealium. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -12,4 +12,6 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+
 @end
+
